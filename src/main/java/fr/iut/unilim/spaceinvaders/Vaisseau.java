4d2b35f6eded7af2388cb93e2682220ef8146a37
Azfar Mehmood <azfar.mehmood98@gmail.com>
2019-05-06 17:59:25 +0200

    refactoring Sprite

--- src/main/java/fr/iut/unilim/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/iut/unilim/spaceinvaders/Vaisseau.java
@@ -1,18 +1,6 @@
 package fr.iut.unilim.spaceinvaders;
 
 public class Vaisseau extends Sprite {
-
-	 public Vaisseau(int longueur, int hauteur) {
-			this(longueur, hauteur, 0, 0);
-		}
-
-		public Vaisseau(int longueur, int hauteur, int x, int y) {
-			this(new Dimension(longueur, hauteur), new Position(x, y));
-		}
-
-		public Vaisseau(Dimension dimension, Position positionOrigine) {
-			this(dimension, positionOrigine, 1);
-		}
 		
 		public Vaisseau(Dimension dimension, Position positionOrigine, int vitesse) {
 			super(dimension, positionOrigine, vitesse);
