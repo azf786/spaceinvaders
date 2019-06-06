699f7cb8a7d548bb0442a95dc117b6ecc02764be
Azfar Mehmood <azfar.mehmood98@gmail.com>
2019-06-06 05:58:58 +0200

    Suppression des commentaires

--- src/main/java/fr/unilim/iut/spaceinvaders/model/Sprite.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/model/Sprite.java
@@ -48,13 +48,7 @@ public abstract class Sprite {
 		   return this.origine.abscisse();
 	   }
 
-	/*public void seDeplacerVersLaDroite() {
-		   this.origine.changerAbscisse(this.origine.abscisse() + vitesse);
-	   }
-
-	public void seDeplacerVersLaGauche() {
-		   this.origine.changerAbscisse(this.origine.abscisse() - vitesse);
-	   }*/
+	
 	public void deplacerHorizontalementVers(Direction direction) {
 		this.origine.changerAbscisse(this.origine.abscisse() + direction.valeur()*vitesse);
 	}
